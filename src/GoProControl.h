0603c0e01d28a0fa2363cd4681e3b87bcf9f33d6
konradit <konradit@users.noreply.github.com>
2018-07-21 19:27:14 +0200

    Add support for Hero 4 Black/Silver, Hero5 Black/Session, Hero6, Hero Session cameras

--- src/GoProControl.h
+++ src/GoProControl.h
@@ -38,9 +38,13 @@
 #define TIMER_MODE 4
 #define PLAY_HDMI 5
 
+//Hero4/5/6 modes:
+#define MULTISHOT_MODE 2
+
 /*Orientation*/
 #define ORIENTATION_UP 0
 #define ORIENTATION_DOWN 1
+#define ORIENTATION_AUTO 2 /*Hero4/5/6*/
 
 /*Resolutions*/
 #define VR_WVGA60 0
@@ -50,6 +54,19 @@
 #define VR_960_30 4
 #define VR_960_48 5
 #define VR_1080_30 6
+
+/*Resolutions Hero4/5/6*/
+
+#define VR_4K 1
+#define VR_2K 4
+#define VR_2K_SuperView 5
+#define VR_1440p 7
+#define VR_1080p_SuperView 8
+#define VR_1080p 9
+#define VR_960p 10
+#define VR_720p_SuperView 11
+#define VR_720p 12
+#define VR_WVGA 13
 
 /*Frame Rates*/
 #define FPS12 0
@@ -65,10 +82,24 @@
 #define FPS120 10
 #define FPS240 11
 
+/* Frame rates Hero4/5/6*/
+
+#define FR_240 0
+#define FR_120 1
+#define FR_100 2
+#define FR_90 3
+#define FR_80 4
+#define FR_60 5
+#define FR_50 6
+#define FR_48 7
+#define FR_30 8
+#define FR_25 9
+
 /*FOV*/
 #define WIDE_FOV 0
 #define MEDIUM_FOV 1
 #define NARROW_FOV 2
+#define LINEAR_FOV 4 /* Hero4/5/6 */
 
 /*Photo Resolutions*/
 #define PR_12mpW 0
@@ -77,6 +108,15 @@
 #define PR_7mpW 3
 #define PR_5mpW 4
 
+/* Photo Resolution Hero 4/5/6 */
+
+#define PR_12MP_Wide 0
+#define PR_12MP_Linear 10
+#define PR_12MP_Medium 8
+#define PR_12MP_Narrow 9
+#define PR_7MP_Wide 1
+#define PR_7MP_Medium 2
+#define PR_5MP_Wide 3
 
 class GoProControl{
 	
