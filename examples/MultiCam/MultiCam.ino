da0c369472b449784b58086741251588a4cc1553
konradit <konradit@users.noreply.github.com>
2018-12-26 13:56:22 +0100

    Add Multi Cam example

---
+++ examples/MultiCam/MultiCam.ino
@@ -0,0 +1,40 @@
+#include <GoProControl.h>
+#include <WiFiUdp.h>
+#include "Constants.h"
+
+GoProControl Hero_Seven(GOPRO_SSID, GOPRO_PASS, HERO6);
+
+GoProControl Hero_Four(GOPRO_SSID_2, GOPRO_PASS_2, HERO4);
+
+uint8_t onStatus = true;
+char in = 0;
+byte macAddr[] = { 0x78, 0x0C, 0xB8, 0xAE, 0xAE, 0xB1 };
+WiFiUDP udp;
+
+void setup() {
+  Serial.begin(115200);
+  Hero_Seven.enableDebug(true);
+  Hero_Four.enableDebug(true);
+  while (!Serial);
+  Serial.println("starting");
+  udp.begin(9);
+  
+  
+}
+
+void loop() {
+  // Shoot
+  	    Hero_Seven.begin();
+            delay(4000);
+       if(!Hero_Seven.startCapture()){
+        Hero_Seven.begin();
+       }
+     delay(4000);
+       Hero_Four.begin();
+delay(4000);
+        Hero_Four.startCapture();
+      
+	delay(4000);
+	
+	
+}
